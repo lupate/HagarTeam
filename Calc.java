08803865d34b3a37af942a2989b1203284eb0fd7
Mr. Lupate <lupate@gmail.com>
2015-12-12 22:32:31 +0400

    adding add method

--- Calc.java
+++ Calc.java
@@ -4,7 +4,12 @@ public class Calc{
 		System.out.println("Instantianting an Object .. No Args");
 	}
 	
+	public int add (int x, int y){
+		return x + y;
+	}
+	
 	public static void main (String... argv){
 		Calc calculator = new Calc();		
+		System.out.println(calculator.add(10, 5));
 	}
 }
