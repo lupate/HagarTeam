478367bbb9fffdf60ea59115f13ff61d36e75b41
Mr. Lupate <lupate@gmail.com>
2015-12-12 22:35:14 +0400

    adding add method

--- Calc.java
+++ Calc.java
@@ -8,8 +8,21 @@ public class Calc{
 		return x + y;
 	}
 	
+	public int sub (int x, int y){
+		if( x < y)
+			return x - y;
+		else
+			return 0;
+	}
+	
 	public static void main (String... argv){
 		Calc calculator = new Calc();		
 		System.out.println(calculator.add(10, 5));
+		
+		int result = calculator.sub(5, 15);
+		if(result > 0)
+			System.out.println(result);
+		else
+			System.out.println("Your subtraction (first - Second) >> first must be greater than Second");
 	}
 }
