e17ab5a05a5e67f3028d756d7b5b08d8bb099db2
Mr. Lupate <lupate@gmail.com>
2015-12-12 22:30:13 +0400

    intial commit by adding my first step in project files

---
+++ Calc.java
@@ -0,0 +1,10 @@
+public class Calc{
+	
+	Calc(){
+		System.out.println("Instantianting an Object .. No Args");
+	}
+	
+	public static void main (String... argv){
+		Calc calculator = new Calc();		
+	}
+}
